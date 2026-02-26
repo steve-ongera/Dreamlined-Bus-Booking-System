1f08078a009c022ab1ba7e4376ce83a2aa3a68f2
steve-ongera <gadafisteve001@gmail.com>
2026-02-26 19:07:10 +0300

    updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,6 +99,23 @@ USE_I18N = True
 
 USE_TZ = True
 
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,
+}
 
 # CORS - React frontend
 CORS_ALLOWED_ORIGINS = [
